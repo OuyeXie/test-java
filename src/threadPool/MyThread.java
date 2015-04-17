9d3fc235f7fe02c519ac660f341f12f7ecac9943
Andy Xie (Xie Ouye) <xieouye@zhihuiya.com>
2015-04-17 10:36:57 +0800

    thread pool customize

--- src/threadPool/MyThread.java
+++ src/threadPool/MyThread.java
@@ -3,6 +3,13 @@ package threadPool;
 public class MyThread extends Thread {
 	@Override
 	public void run() {
-		System.out.println(Thread.currentThread().getName() + "正在执行。。。");
+		System.out.println(Thread.currentThread().getName() + " starts。。。");
+		try {
+			sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(Thread.currentThread().getName() + " stops。。。");
 	}
 }
