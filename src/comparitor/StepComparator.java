8af522a0852d7223c2872c780cff752babe1778d
Andy Xie (Xie Ouye) <xieouye@zhihuiya.com>
2015-04-25 14:41:38 +0800

    add comparator

---
+++ src/comparitor/StepComparator.java
@@ -0,0 +1,28 @@
+package comparitor;
+
+import java.util.Comparator;
+import java.util.Date;
+
+/**
+ * 对Step类进行排序
+ * 
+ * @author Administrator
+ *
+ */
+public class StepComparator implements Comparator<Step> {
+
+	/**
+	 * 如果o1小于o2,返回一个负数;如果o1大于o2，返回一个正数;如果他们相等，则返回0;
+	 */
+	@Override
+	public int compare(Step o1, Step o2) {
+		Date acceptTime1 = UtilTool.strToDate(o1.getAcceptTime(), null);
+		Date acceptTime2 = UtilTool.strToDate(o2.getAcceptTime(), null);
+
+		// 对日期字段进行升序，如果欲降序可采用before方法
+		if (acceptTime1.after(acceptTime2))
+			return 1;
+		return -1;
+	}
+
+}
