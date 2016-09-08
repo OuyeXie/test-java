617dd075377bdef463e902c2a11d09a142700b83
Ouye Xie <o@shengupiao.com>
2016-09-08 17:33:49 +0800

    another solution

--- src/main/java/test/MainAmazon.java
+++ src/main/java/test/MainAmazon.java
@@ -66,6 +66,32 @@ public class MainAmazon {
         return result;
     }
 
+    static boolean check(int cityLength, int cityWidth, int lockerXCoordinate, int lockerYCoordinate) {
+        if (lockerXCoordinate < 0 || lockerYCoordinate < 0 || lockerXCoordinate >= cityLength || lockerYCoordinate >= cityWidth) {
+            return false;
+        }
+        return true;
+    }
+
+
+    static Set<int[]> neighbours(int cityLength, int cityWidth, int lockerXCoordinate, int lockerYCoordinate, int distance) {
+        Set<int[]> results = new HashSet<>();
+        int currX;
+        int currY;
+        for (int i = -distance; i <= distance; i++) {
+            int j = distance - Math.abs(i);
+            if (check(cityLength, cityWidth, lockerXCoordinate + i, lockerYCoordinate + j)) {
+                results.add(new int[]{lockerXCoordinate + i, lockerYCoordinate + j});
+            }
+            j = -j;
+            if (check(cityLength, cityWidth, lockerXCoordinate + i, lockerYCoordinate + j)) {
+                results.add(new int[]{lockerXCoordinate + i, lockerYCoordinate + j});
+            }
+        }
+        return results;
+    }
+
+    // TODO: better stop criteria
     static int[][] getLockerDistanceGrid2(int cityLength, int cityWidth, int[] lockerXCoordinates, int[] lockerYCoordinates) throws Exception {
         if (cityLength > 9 || cityLength < 1) {
             throw new Exception("out of range");
@@ -76,33 +102,24 @@ public class MainAmazon {
         if (lockerXCoordinates.length != lockerYCoordinates.length) {
             throw new Exception("array length mismatch");
         }
-        Map<String, List<Integer>> coorToDistances = new HashMap<>();
-        for (int n = 0; n < lockerXCoordinates.length; n++) {
-            for (int i = 0; i < cityLength; i++) {
-                for (int j = 0; j < cityWidth; j++) {
-                    String key = String.format("%d:%d", i, j);
-                    if (coorToDistances.containsKey(key)) {
-                        coorToDistances.get(key).add(distance(i, j, lockerXCoordinates[n], lockerYCoordinates[n]));
-                    } else {
-                        List<Integer> distances = new ArrayList<>(lockerXCoordinates.length);
-                        distances.add(distance(i, j, lockerXCoordinates[n], lockerYCoordinates[n]));
-                        coorToDistances.put(key, distances);
+        int[][] result = new int[cityLength][cityWidth];
+        int[][] check = new int[cityLength][cityWidth];
+        int total = 0;
+        int distance = 0;
+        while (total < cityLength * cityWidth) {
+            for (int n = 0; n < lockerXCoordinates.length; n++) {
+                int currX = lockerXCoordinates[n];
+                int currY = lockerYCoordinates[n];
+                Set<int[]> neighbours = neighbours(cityLength, cityWidth, currX, currY, distance);
+                for (int[] neighbour : neighbours) {
+                    if (check[neighbour[0]][neighbour[1]] == 0) {
+                        check[neighbour[0]][neighbour[1]] = 1;
+                        result[neighbour[0]][neighbour[1]] = distance;
+                        total++;
                     }
                 }
             }
-        }
-        int[][] result = new int[cityLength][cityWidth];
-        for (int i = 0; i < cityLength; i++) {
-            for (int j = 0; j < cityWidth; j++) {
-                String key = String.format("%d:%d", i, j);
-                if (coorToDistances.containsKey(key)) {
-                    List<Integer> distances = coorToDistances.get(key);
-                    int distance = min(distances);
-                    result[i][j] = distance;
-                } else {
-                    result[i][j] = 0;
-                }
-            }
+            distance++;
         }
         return result;
     }
@@ -189,9 +206,58 @@ public class MainAmazon {
     }
 
     public static void main(String[] args) {
+
+        // small test for stringbuilder
+        System.out.println("//////////////////////////////////////////");
         StringBuilder s = new StringBuilder();
         s.append("asdf");
         System.out.println(s.toString());
+        // small test for array
+        System.out.println("//////////////////////////////////////////");
+        int[][] results = new int[2][2];
+        for (int[] rr : results) {
+            for (int rrr : rr) {
+                System.out.print(rrr);
+                System.out.print(" ");
+            }
+            System.out.print("\n");
+        }
+
+        //run test 1
+        System.out.println("//////////////////////////////////////////");
+        int cityLength = 9;
+        int cityWidth = 9;
+        int[] lockerXCoordinates = new int[]{1, 8};
+        int[] lockerYCoordinates = new int[]{1, 8};
+        try {
+            int[][] r = getLockerDistanceGrid(cityLength, cityWidth, lockerXCoordinates, lockerYCoordinates);
+            for (int[] rr : r) {
+                for (int rrr : rr) {
+                    System.out.print(rrr);
+                    System.out.print(" ");
+                }
+                System.out.print("\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("//////////////////////////////////////////");
+
+        try {
+            int[][] r = getLockerDistanceGrid2(cityLength, cityWidth, lockerXCoordinates, lockerYCoordinates);
+            for (int[] rr : r) {
+                for (int rrr : rr) {
+                    System.out.print(rrr);
+                    System.out.print(" ");
+                }
+                System.out.print("\n");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
