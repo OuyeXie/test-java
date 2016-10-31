914f4082a42f1d3d3c38d6279dc2e8c4c1ebb2a9
Ouye Xie <o@shengupiao.com>
2016-10-31 15:42:56 +0800

    update

---
+++ src/main/java/test/TestInterface.java
@@ -0,0 +1,28 @@
+package test;
+
+interface Interface1 {
+	public static final int a = 1;
+	public static final int b = 11;
+
+	public void print ();
+}
+
+interface Interface2 {
+	public static final int a = 2;
+	public static final int c = 22;
+
+	public void print ();
+}
+
+public class TestInterface implements Interface1, Interface2{
+
+	public void print () {
+		System.out.println(this.a);
+		System.out.println(this.b);
+		System.out.println(this.c);
+	}
+	public static void main(String[] args) {
+		TestInterface testInterface = new TestInterface();
+		testInterface.print();
+	}
+}
