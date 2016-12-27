b9efa67f61978c071b4b57ed55217e474cbb1593
Ouye Xie <o@shengupiao.com>
2016-12-27 15:23:48 +0800

    update

--- src/main/java/test/CoModificationTest.java
+++ src/main/java/test/CoModificationTest.java
@@ -1,40 +1,72 @@
 package test;
 
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by ouyexie on 27/12/2016.
  */
 public class CoModificationTest {
-    static void main(String[] args) {
-        List<Integer> list = new LinkedList<>();
-        for (int i = 0; i < 100; i++) {
-            list.add(i);
+
+    private static List<Integer> list;
+    private static Iterator<Integer> iterator;
+    private static int count = 0;
+
+    static class MyThread extends Thread {
+        public MyThread(String name) {
+            super(name);
         }
-        Iterator<Integer> iterator = list.iterator();
 
-        new Thread("Thread 0"){
-            @Override
-            public void run() {
-                while (iterator.hasNext()){
+        @Override
+        public void run() {
+            try {
+                while (iterator.hasNext()) {
                     int i = iterator.next();
-                    System.out.println(this.getName() + " : " + i);
+//                        iterator.remove();
+                    synchronized (CoModificationTest.class) {
+                        System.out.println(count + " : " + this.getName() + " : " + i);
+                        count++;
+                    }
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        System.out.println(this.getName() + " interrupted!!!");
+                    }
                 }
                 System.out.println(this.getName() + " finished!!!");
+            } catch (ConcurrentModificationException ee) {
+                System.out.println(this.getName() + " error!!!");
+                ee.printStackTrace();
             }
-        }.start();
+        }
+    }
 
-        new Thread("Thread 1"){
-            @Override
-            public void run() {
-                while (iterator.hasNext()){
-                    int i = iterator.next();
-                    System.out.println(this.getName() + " : " + i);
-                }
-                System.out.println(this.getName() + " finished!!!");
+
+    public static void main(String[] args) {
+        try {
+            list = new LinkedList<>();
+            for (int i = 0; i < 100; i++) {
+                list.add(i);
             }
-        }.start();
+            iterator = list.iterator();
+
+            Thread thread0 = new MyThread("Thread-Name-0");
+            Thread thread1 = new MyThread("Thread-Name-1");
+
+            ExecutorService service = Executors.newCachedThreadPool();
+            service.submit(thread0);
+            service.submit(thread1);
+            service.shutdown();
+            service.awaitTermination(100 * 1000, TimeUnit.MILLISECONDS);
+
+            System.out.println("final count : " + count);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
